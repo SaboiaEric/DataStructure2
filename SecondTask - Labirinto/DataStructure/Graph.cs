e4ef27a4b8e2f497bdf5073be268d34ee15e57e5
Eric Saboia <Eric Saboia>
2017-02-27 21:54:03 -0300

    Add second project: Labirinto

---
+++ SecondTask - Labirinto/DataStructure/Graph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGrafos.DataStructure
+{
+    /// <summary>
+    /// Classe que representa um grafo.
+    /// </summary>
+    public class Graph
+    {
+
+
+    }
+}
